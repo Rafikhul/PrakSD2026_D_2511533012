2109209aa8542e293090fe03261f5764187ad27e
Khul <159623218+Rafikhul@users.noreply.github.com>
2026-04-07 11:55:23 +0700

    pratikum pekan2

--- PrakSD2026_D_2511533012/src/Pekan2_2511533012/DaftarKataDriver_2511533012.java
+++ PrakSD2026_D_2511533012/src/Pekan2_2511533012/DaftarKataDriver_2511533012.java
@@ -4,7 +4,26 @@ public class DaftarKataDriver_2511533012 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DaftarKata_2511533012 al = new DaftarKata_2511533012();
+		
+		al.tambah_2511533012("Kami");
+		al.tambah_2511533012("Informatika");
+		
+		al.tambahPada_2511533012(1, "Mahasiswa");
+		
+		System.out.println("Awal	: " + al);
+		
+		al.ubahElemen_2511533012(1, "Departmen");
+		System.out.println("Setelah Ubah: " + al);
+		
+		String terhapus = al.hapusElemen_2511533012(0);
+		System.out.println("Terhapus	: " + terhapus);
+		System.out.println("Setelah Hapus : " + al);
+		
+		System.out.println("Iterasi");
+		al.iterasiCetak_2511533012();
+		System.out.println();
+		
 	}
 
 }
--- PrakSD2026_D_2511533012/src/Pekan2_2511533012/DaftarKata_2511533012.java
+++ PrakSD2026_D_2511533012/src/Pekan2_2511533012/DaftarKata_2511533012.java
@@ -1,10 +1,41 @@
 package Pekan2_2511533012;
+import java.util.ArrayList;
 
 public class DaftarKata_2511533012 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	private final ArrayList<String> data;
+		
+		public DaftarKata_2511533012() {
+			this.data = new ArrayList<>();
+		}
+		
+		public void tambah_2511533012(String elemen) {
+			data.add(elemen);
+		}
+		
+		public void tambahPada_2511533012(int index, String elemen) {
+			data.add(index, elemen);
+		}
+		
+		public void ubahElemen_2511533012(int index, String nilaiBaru) {
+			data.set(index, nilaiBaru);
+		}
+		
+		public String hapusElemen_2511533012(int index) {
+			return data.remove(index);
+		}
+		
+		public void iterasiCetak_2511533012() {
+			for (int i = 0; i < data.size(); i++) {
+				System.out.println(data.get(i) + " ");
+			}
+		}
+		
+		public String get(int index) {
+			return data.get(index);
+		}
+		
+		@Override
+		public String toString() {
+			return data.toString();
+		}
 }
